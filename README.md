f3638fa2cc80c72cb4d5b2600b8a58011692d15a
Preet <chamanbawa12@gmail.com>
2022-12-12 14:43:53 -0600

    margin top added

---
+++ README.md
@@ -0,0 +1,8 @@
+## Click me if you can
+
+You lost?
+
+### Demo
+
+Click [here](https://chamanbawa.github.io/map-tracker/) Find yourself. 
+
--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
+import { onEvent, getElement, select, print } from "./utils.js";
+
+const loading = select('.loading');
 
 function getLocation(position) {
+
+    loading.style.visibility = 'hidden';
     const { latitude, longitude } = position.coords;
 
     mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhbWFucHJlZXRiYXdhIiwiYSI6ImNsYmdyeDNhZzBodHYzb211MnNjNW43bjcifQ.LrFSdfTtOaY4wbKCUeXeWg';
@@ -9,19 +14,16 @@ function getLocation(position) {
         container: 'map',
         style: 'mapbox://styles/chamanpreetbawa/clbl4dyy7000a14o50895o0fc',
         zoom: 15,
-
         center: [longitude, latitude],
-
     });
-// Create a new marker.
-const marker = new mapboxgl.Marker({
-    color: "#66bacf",
+
+    const marker = new mapboxgl.Marker({
+        color: "#66bacf",
     }).setLngLat([longitude, latitude])
-    .addTo(map);
+        .addTo(map);
 
 }
 
-// Create a new marker.
 
 function errorHandler(error) {
     console.log(error.message);
@@ -31,15 +33,11 @@ const options = {
     enableHighAccuracy: true
 };
 
-
+onEvent('load',window, () =>{
 if (navigator.geolocation) {
     let geo = navigator.geolocation;
     geo.getCurrentPosition(getLocation, errorHandler, options);
 } else {
     console.log('Geo Location is not suppprted by your browser');
 }
-
-
-
-
-
+});
